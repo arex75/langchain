2bd32cd241f95b42d2be3cd5893f634b27039fe1
arex75 <arya.aghahosseini@gmail.com>
2024-11-11 17:35:16 +0330

    modified test_server file

--- test_server.py
+++ test_server.py
@@ -5,7 +5,7 @@ class WebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        print("Received webhook:", post_data.decode('utf-8'))
+        print('Received webhook:', post_data.decode('utf-8'))
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
